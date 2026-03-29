8049b473ed618efe85013ebdcd72d974b56608bd
Shashwat Tejaswi <shashwatteju06@gmail.com>
2026-03-29 21:38:08 +0530

    learning backend in next js create GET route use dynamic route to return a dynamic object through json folder

---
+++ app/todos/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { readFile, writeFile } from "node:fs/promises";
+import {todos} from "./testData.json"
+export async function GET(nextRequest:Request)
+{
+    console.log(nextRequest.url);
+    
+    // return new Response(JSON.stringify({message:"ok nothing happening",type:"lets go...."}),{
+    //     status:201,
+    //     statusText:"ban gya bhaii",
+    //     headers:{
+    //         "Content-Type":"image/png"
+    //     }
+    // });
+
+    return NextResponse.json({message:"OK",data:[...todos]})
+}
